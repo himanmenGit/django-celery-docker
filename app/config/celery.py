ed34742478e6350c0d970d9519ea9085b2cacb96
himanmenGit <study.himanmen@gmail.com>
2018-11-08 15:46:18 +0900

    django-celery추가 및 demoapp.tasks를 통한 celery task 구현 브로커 redis

---
+++ app/config/celery.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+app = Celery('tasks', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
+
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
