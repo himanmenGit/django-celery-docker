e4a9c43a3ba064386a4ba9fcac367234e7d17f1f
himanmenGit <study.himanmen@gmail.com>
2018-11-08 16:55:51 +0900

    django-celery가 아닌 celery4.2버전으로 작동

--- app/config/celery.py
+++ app/config/celery.py
@@ -1,14 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = Celery('tasks', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
 
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
 
 app.conf.update(
     CELERY_ACCEPT_CONTENT=['json'],
--- app/config/views.py
+++ app/config/views.py
@@ -3,5 +3,5 @@ from demoapp.tasks import add
 
 
 def home(request):
-    res = add.delay(2, 20)
+    res = add.delay(2, 50)
     return HttpResponse(res.get())
